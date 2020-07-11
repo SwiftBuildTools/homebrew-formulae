4b15c5db6a8218975b351f3eeaa6de462ee2d7bd
Scott Robbins <scott.robbins@mobiledefense.com>
2020-07-11 17:18:21 -0400

    Update changes formula

--- Formula/changes.rb
+++ Formula/changes.rb
@@ -1,18 +1,16 @@
 class Changes < Formula
-  desc "A utility for managing and generating changelogs"
+  desc "A CLI tool for creating Changelog entries and generating Changelogs and other documents from those."
   homepage "https://github.com/SwiftBuildTools/Changes"
   head "https://github.com/SwiftBuildTools/Changes.git"
   version "0.1.0"
-  url "https://github.com/SwiftBuildTools/Changes/archive/#{version}.tar.gz"
-  sha256 "26816903a36478c746c1d0c572388997983726d2147fa68c30660ff5a9037683"
+  url "https://github.com/SwiftBuildTools/Changes/releases/download/v#{version}/changes-swift-5-2-macOS.tar.gz"
+  sha256 "a9132b46a1a55b1153c1b9c189d8772f4c2b90a1ea3ffa01e0208ad1ce272d7a"
 
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-    bin.install ".build/release/changes"
+    bin.install "changes"
   end
 
   test do
     system "changes" "--help"
   end
 end
-  
